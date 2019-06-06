90e9590f664df953553d2fd352be582eb6fe6e23
David Pareti <50494412+DavidPareti@users.noreply.github.com>
2019-06-06 15:43:31 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.scss';
 import Header from './Header';
 import Footer from './Footer';
 import './scss/main.css';
-import Cocktail from './img/Cocktail.svg';
 import Orders from './pages/Orders';
 import News from './pages/News';
 import Advise from './Advise';
