58ed3690f2a8b1f556af0dbe36cfd2d38b93e9cb
David Pareti <50494412+DavidPareti@users.noreply.github.com>
2019-06-13 10:18:14 +0200

    Update

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,12 +1,11 @@
 import React from 'react'
 class Total extends React.Component {
-  
   render() {
 
      return (
       <div className = {this.props.total}>
           <p className = "p_total" >{this.props.p_total}</p>
-          <p className = "price_total" >{this.props.price_total}</p>
+          <p className = "price_total" >{this.props.price_total} €</p>
       </div>
 
     );
